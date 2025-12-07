b8de8acd25cd9222a3113e40ce8ff89f5a205857
pqrm1 <joshipratik0w0@gmail.com>
2025-12-08 01:04:47 +0545

    Add files via upload

--- Med-Info/hospital_mngt/hospital/urls.py
+++ Med-Info/hospital_mngt/hospital/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from hospital.views import About,Home,Contact,Chatbot
 
 urlpatterns = [
-    path('',Home,name='home'),
+    path(''or'home/',Home,name='home'),
     path('about/', About,name='about'),
     path('contact/', Contact,name='contact'),
     path('chatbot/', Chatbot,name='chatbot'),
